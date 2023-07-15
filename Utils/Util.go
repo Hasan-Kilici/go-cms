eb544e5bd869d6b2ee8add30e9f8b2ee6d399ce8
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2023-07-16 00:54:38 +0300

    Update Util.go

--- Utils/Util.go
+++ Utils/Util.go
@@ -8,6 +8,8 @@ import(
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 type File struct {
@@ -71,4 +73,47 @@ func GetFiles(folderPath string) ([]File, error) {
 	}
 
 	return fileStructs, nil
+}
+
+func ValidateEmail(mail string) bool {
+	regex := regexp.MustCompile(`^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$`)
+	return regex.MatchString(mail)
+}
+
+func ValidatePassword(password string) bool {
+	var error string
+
+	if strings.Contains(password, " ") {
+		error = "Şifre boşluk içeremez"
+	}
+
+	if len(password) < 8 {
+		error = "Şifre en az 8 karakter olmak zorundadır."
+	}
+
+	if len(password) > 32 {
+		error = "Şifre en fazla 32 karakter olmalıdır."
+	}
+
+	if !regexp.MustCompile(`\d`).MatchString(password) {
+		error = "Şifrede sayılar yer almalıdır."
+	}
+
+	if !regexp.MustCompile(`[a-zA-Z]`).MatchString(password) {
+		error = "Şifrede metin bulunmak zorundadır."
+	}
+
+	if !regexp.MustCompile(`[!@#$%^&*(),.?":{}\|_<>]`).MatchString(password) {
+		error = "Şifrede özel karakter bulunmak zorundadır."
+	}
+
+	if error != "" {
+		fmt.Println(error)
+	}
+
+	return error == ""
+}
+
+func ValidateUsername(username string) bool {
+	return !strings.Contains(username, " ")
 }
