4ca5fe3c04d84739768e12108811f939e2698b2c
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2023-07-16 00:53:21 +0300

    Update Post.go

--- Handlers/Post.go
+++ Handlers/Post.go
@@ -3,6 +3,7 @@ package Handlers
 import (
 	"github.com/gofiber/fiber/v2"
 	"CMS/Database"
+	"CMS/Utils"
 	"time"
 )
 
@@ -23,6 +24,30 @@ func Register(c *fiber.Ctx) error {
 	form := new(RegisterForm)
 	if err := c.BodyParser(form); err != nil {
 		return err
+	}
+
+	if !Utils.ValidateEmail(form.Email){
+		c.Render("register", fiber.Map{
+			"title":"Giriş yap",
+			"error":"Bu Email geçersiz",
+		})
+		return nil
+	}
+
+	if !Utils.ValidatePassword(form.Pass) {
+		c.Render("register", fiber.Map{
+			"title":"Giriş yap",
+			"error":"Şifre güçlü değil",
+		})
+		return nil
+	}
+
+	if !Utils.ValidateUsername(form.Username) {
+		c.Render("register", fiber.Map{
+			"title":"Giriş yap",
+			"error":"Kullanıcı adı boşluk içeremez",
+		})
+		return nil
 	}
 
 	if !Database.Register(form.Username, form.Email, form.Pass){
