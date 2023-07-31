13be4bfb12472a7ec1c933f2a50c3e1d7c635c5d
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2023-07-31 21:21:12 +0300

    Update List.go

--- Database/List.go
+++ Database/List.go
@@ -4,56 +4,6 @@ import (
     "fmt"
 )
 
-type Users struct{
-	ID			int
-	Token		string
-	Username	string
-	Email		string
-	Password	string
-	Perm		string
-}
-
-type Blogs struct {
-	ID				int
-	Token			string
-	Title			string
-	HTML			string
-	Views			int
-	Like			int
-}
-
-type Tags struct {
-    ID              int
-    Token           string
-    Tag             string
-    BlogToken       string
-    ProductToken    string
-}
-
-
-type GaleryItem struct {
-	Path        string
-	Title       string
-	Description string
-    Token       string
-}
-
-type Products struct {
-    ID              int
-    Token           string
-    Name            string
-    Price           int
-    Description     string
-    ImagePath       string
-}
-
-type ProductPhoto struct {
-    ID              int
-    Token           string
-    ProductToken    string
-    Path            string
-}
-
 func ListAllBlogs() ([]Blogs, error) {
     query := "SELECT * FROM blogs"
     rows, err := db.Query(query)
